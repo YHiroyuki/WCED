ab8da032b6a224542155d98b3fdd12ee69a78a11
hiroyuki <y.hiro4823gta@gmail.com>
2020-08-17 23:38:38 +0900

    マージソートの実装

---
+++ sort/merge_sort.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"math"
+)
+
+func main() {
+}
+
+func MergeSort(arr []int) []int {
+	length := len(arr)
+	if length <= 1 {
+		return arr
+	}
+
+	center := int(math.Ceil(float64(length) / 2.0))
+	beforeArr := MergeSort(arr[0:center])
+	afterArr := MergeSort(arr[center:length])
+
+	newArr := []int{}
+
+	for {
+		beforeLength := len(beforeArr)
+		afterLength := len(afterArr)
+		if beforeLength == 0 && afterLength == 0 {
+			break
+		} else if 0 < beforeLength && afterLength == 0 {
+			newArr = append(newArr, beforeArr[0])
+			beforeArr = beforeArr[1:]
+		} else if beforeLength == 0 && 0 < afterLength {
+			newArr = append(newArr, afterArr[0])
+			afterArr = afterArr[1:]
+		} else {
+			if afterArr[0] < beforeArr[0] {
+				newArr = append(newArr, afterArr[0])
+				afterArr = afterArr[1:]
+			} else {
+				newArr = append(newArr, beforeArr[0])
+				beforeArr = beforeArr[1:]
+			}
+		}
+	}
+
+	return newArr
+}
