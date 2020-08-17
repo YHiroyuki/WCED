88433fad96e2a46844be22b9bf843e87780d8bc3
hiroyuki <y.hiro4823gta@gmail.com>
2020-08-17 23:39:42 +0900

    雑に動作確認

--- sort/merge_sort.go
+++ sort/merge_sort.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"math"
+	"math/rand"
+	"time"
 )
 
 func main() {
+	sample := []int{}
+	for i := 0; i < 20; i++ {
+		sample = append(sample, i)
+	}
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(sample), func(i, j int) { sample[i], sample[j] = sample[j], sample[i] })
+	fmt.Println(sample)
+
+	ans := MergeSort(sample)
+
+	fmt.Println(ans)
 }
 
 func MergeSort(arr []int) []int {
