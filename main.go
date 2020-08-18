022bfc73f84e77b2c7740711e08993bbf62aa0d0
hiroyuki <y.hiro4823gta@gmail.com>
2020-08-18 21:33:06 +0900

    main関数からサンプルロジックの削除

--- main.go
+++ main.go
@@ -1,24 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	"time"
-
-	"github.com/YHiroyuki/WCED/sort"
-)
-
 func main() {
 
-	sample := []int{}
-	for i := 0; i < 20; i++ {
-		sample = append(sample, i)
-	}
-	rand.Seed(time.Now().UnixNano())
-	rand.Shuffle(len(sample), func(i, j int) { sample[i], sample[j] = sample[j], sample[i] })
-	fmt.Println(sample)
-
-	ans := sort.MergeSort(sample)
-
-	fmt.Println(ans)
 }
